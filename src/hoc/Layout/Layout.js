01d77884f8d4f81eace068704e229813eecaaa40
Крум Багашев <krum.bagashev@gmail.com>
2021-04-07 12:57:10 +0300

    Small fixes

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -6,7 +6,7 @@ import Sidedrawer from "../../components/Navigation/Sidedrawer/Sidedrawer";
 
 class Layout extends Component {
 	state = {
-		showSideDrawer: true,
+		showSideDrawer: false,
 	};
 
 	sideDrawerClosedHandler = () => {
